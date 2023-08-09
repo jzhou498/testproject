689bbfee388ab3a916595f0d6b34813cfe88ac96
charz07 <charlottezhou6@gmail.com>
2023-08-08 19:17:03 -0700

    Fixed issue where background covers score UI and fixed double jump bug

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     public float jumpStrength;
     public Rigidbody2D myRigidBody;
+    public float inputBuffer;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,13 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true || Input.GetKeyDown(KeyCode.UpArrow) == true)
+        if (inputBuffer == 0)
+            inputBuffer = Time.time;
+
+        if(Time.time - inputBuffer > 0.8f && (Input.GetKeyDown(KeyCode.Space) == true || Input.GetKeyDown(KeyCode.UpArrow) == true))
         {
             myRigidBody.velocity = Vector2.up * jumpStrength;
+            inputBuffer = 0.0f;
         }
     }
 }
